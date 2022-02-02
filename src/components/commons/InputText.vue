b84e9f2dc923ed4c7161b2f215ead9131b4b66f3
LorenzoBernardini88 <lorenzo.bernardini88@gmail.com>
2022-02-02 15:44:20 +0100

    creo repo

---
+++ src/components/commons/InputText.vue
@@ -0,0 +1,13 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+export default {
+    name: 'InputText'
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
---
+++ src/components/macro/Main.vue
@@ -0,0 +1,19 @@
+<template>
+    <main>
+        <InputText/>
+    </main>
+</template>
+
+<script>
+import InputText from "../commons/InputText.vue";
+export default {
+    name:'Main',
+    components:{
+        InputText
+    }
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
