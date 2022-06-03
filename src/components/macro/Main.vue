d51ac3ba99edb68fafbcfd03d45d493e56bc0424
Lorenzo Bernardini <lorenzo.bernardini88@gmail.com>
2022-06-03 17:57:22 +0200

    h1 animation and overflow scrollbar fix

--- src/components/macro/Main.vue
+++ src/components/macro/Main.vue
@@ -1,7 +1,20 @@
 <template>
     <main>
         <div v-if="dataFilm ==0 && dataSerie ==0 " class="intro">
-            <h1>Boolfix</h1>
+            <h1 class="jt --debug">
+            <span class="jt__row">
+                <span class="jt__text">BOOLFLIX</span>
+            </span>
+            <span class="jt__row jt__row--sibling" aria-hidden="true">
+                <span class="jt__text">BOOLFLIX</span>
+            </span>
+            <span class="jt__row jt__row--sibling" aria-hidden="true">
+                <span class="jt__text">BOOLFLIX</span>
+            </span>
+            <span class="jt__row jt__row--sibling" aria-hidden="true">
+                <span class="jt__text">BOOLFLIX</span>
+            </span>
+            </h1>
         </div>
         <div v-else>
             <h1 >Film</h1>
@@ -68,6 +81,75 @@ h1{
     text-align: center;
     margin: 20px 0px;
     font-size: 50px;
+}
+.jt {
+    position: relative;
+    font-size: 20vmin;
+    font-family: 'Staatliches', sans-serif;
+    text-transform: uppercase;
+    font-display: swap;
+    text-shadow: 0 0 10px tomato;
+}
+
+.jt__row {
+    display: block;
+}
+.jt__row:nth-child(1) {
+    clip-path: polygon(-10% 75%, 110% 75%, 110% 110%, -10% 110%);
+}
+.jt__row:nth-child(2) {
+    clip-path: polygon(-10% 50%, 110% 50%, 110% 75.3%, -10% 75.3%);
+}
+.jt__row:nth-child(3) {
+    clip-path: polygon(-10% 25%, 110% 25%, 110% 50.3%, -10% 50.3%);
+}
+.jt__row:nth-child(4) {
+    clip-path: polygon(-10% 0%, 110% 0%, 110% 25.3%, -10% 25.3%);
+}
+
+.jt__row.jt__row--sibling {
+    position: absolute;
+    top: 0;
+    left: 0;
+    user-select: none;
+    witdh:800px;
+}
+
+.jt__text {
+    display: block;
+    transform-origin: bottom left;
+    animation: moveIn 2s 0s cubic-bezier(.36,0,.06,1) alternate infinite;
+}
+.jt__row:nth-child(1) .jt__text {
+    transform: translateY(-0.1em);
+}
+.jt__row:nth-child(2) .jt__text {
+    transform: translateY(-0.3em) scaleY(1.1);
+}
+.jt__row:nth-child(3) .jt__text {
+  transform: translateY(-0.5em) scaleY(1.2) ;
+}
+.jt__row:nth-child(4) .jt__text {
+  transform: translateY(-0.7em) scaleY(1.3) ;
+}
+.jt__row:nth-child(5) .jt__text {
+  transform: translateY(-0.9em) scaleY(1.4) ;
+}
+.jt__row:nth-child(6) .jt__text {
+  transform: translateY(-1.1em) scaleY(1.5) ;
+}
+
+@keyframes moveIn {
+  50%, 100% { 
+    transform: translateY(0em);
+    opacity: 1; 
+    filter: blur(0px);
+  }
+  0%   { 
+  opacity: 0; 
+  filter: blur(10px);
+  
+  }
 }
 
 </style>
